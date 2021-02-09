3e2527d6951e3d5b67a5ffed8522abbc8ccb87a4
taka-elle <takaelle0119@gmail.com>
2021-02-09 16:37:57 +0900

    計算式の短縮
    ２行あった計算式を１行に

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,8 +35,7 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
       
-      wday_num = @todays_date.wday
-      wday_num += x
+      wday_num = @todays_date.wday+x
       if wday_num >= 7
         wday_num = wday_num -7
       end
